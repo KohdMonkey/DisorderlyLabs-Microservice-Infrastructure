c2b591eac2717e55fc666130cbd536e6c8bddd60
Tuan Tran <atran18@ucsc.edu>
2018-02-22 18:15:58 -0800

    new tracing code

--- cart/src/main/java/com/disorderlylabs/cart/CartApplication.java
+++ cart/src/main/java/com/disorderlylabs/cart/CartApplication.java
@@ -11,6 +11,8 @@ import brave.sampler.Sampler;
 import brave.propagation.B3Propagation;
 import brave.propagation.ExtraFieldPropagation;
 
+import org.springframework.web.client.RestTemplate;
+import zipkin2.codec.Encoding;
 import zipkin2.reporter.AsyncReporter;
 import zipkin2.reporter.okhttp3.OkHttpSender;
 
@@ -20,9 +22,17 @@ import zipkin2.reporter.okhttp3.OkHttpSender;
 public class CartApplication {
 
     @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
     public Tracing zipkinTracer() {
-        OkHttpSender sender = OkHttpSender.create("http://127.0.0.1:9411/api/v2/spans");
-        AsyncReporter<zipkin2.Span> spanReporter = AsyncReporter.create(sender);
+        OkHttpSender sender = OkHttpSender.newBuilder()
+                .encoding(Encoding.JSON)
+                .endpoint("http://10.0.0.24:9411/api/v2/spans")
+                .build();
+        AsyncReporter<zipkin2.Span> spanReporter = AsyncReporter.builder(sender).build();
 
         Tracing tracing = Tracing.newBuilder()
                 .localServiceName("application")
